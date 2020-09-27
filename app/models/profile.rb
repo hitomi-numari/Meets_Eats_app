75e5b83112ae9985eea3f6b028db4a84db5c558d
hitomi numari <h.h.hitomi1216@gmail.com>
2020-09-27 16:17:26 +0900

    profileのバリデーション追加とエラー表示の訂正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,10 +2,12 @@ class Profile < ApplicationRecord
   belongs_to :user
   mount_uploader :icon, IconUploader
   validates :icon, presence: true
-  validates :nickname,  presence: true, length: { maximum: 30 }
-  validates :birthday,  presence: true
+  validates :nickname, presence: true, length: { maximum: 30 }
+  validates :birthday, presence: true
   validates :gender, presence: true
   validates :details, presence: true
+  validates :facebook_url, format: URI::regexp(%w(http https))
+  validates :instagram_url, format: URI::regexp(%w(http https))
 
   def calc_age
     age = (Date.today.strftime('%Y%m%d').to_i - birthday.strftime('%Y%m%d').to_i) / 10000
