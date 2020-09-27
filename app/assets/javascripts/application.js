e51637d255df2b2eab21e7c9ce808e3dc0e385dd
hitomi numari <h.h.hitomi1216@gmail.com>
2020-09-27 09:31:28 +0900

    webpacker installed & css訂正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require jquery/dist/jquery.js
-//= require popper.js/dist/popper.js
-//= require bootstrap/dist/js/bootstrap.min
-//= require_tree .
+// require rails-ujs
+// require jquery/dist/jquery.js
+// require popper.js/dist/popper.js
+// require bootstrap/dist/js/bootstrap.min
+// require_tree .
